531cbb991faaefa6ce6faff7079b74db39e6df60
arturdementjev29-sys <arturdementjev29@gmail.com>
2025-10-22 17:01:21 +0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,61 @@
+public class PrinterApp {
+    public static void main(String[] args) {
+
+        String creator = "Artur";
+        int number1 = 5;
+        int number2 = 2;
+        int multiplication;
+        int addition;
+        int division;
+        int subtraction;
+
+        System.out.println("Printer App");
+
+        System.out.print("This app is made by: ");
+        System.out.println(creator);
+
+
+        System.out.println("The value of number1 is " + number1);
+
+        System.out.println("The value of number2 is " + number2);
+
+        multiplication = number1 * number2;
+        System.out.println(number1 + " * " + number2 + " = " + multiplication);
+
+        addition = number1 + number2;
+        System.out.println(number1 + " + " + number2 + " = " + addition);
+
+        division = number1 / number2; 
+        System.out.println(number1 + " / " + number2 + " = " + division);
+
+        subtraction = number1 - number2;
+        System.out.println(number1 + " - " + number2 + " = " + subtraction);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+
+
+
+
+     
+
+
+
+
+
+
+
